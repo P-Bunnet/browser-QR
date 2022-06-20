bcb01aaab03162d74560631561374c3178d52a07
Phoung Bunnet <bunnet@pop-os.localdomain>
2022-06-20 17:16:23 +0700

    function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import QrScanner from "qr-scanner";
+import QRCode from "qrcode";
 function App() {
   const [id, setID] = useState();
   const [image, setImage] = useState();
   const [message, setMessage] = useState();
+  const [text, setText] = useState();
   function test() {
     /* eslint-disable no-undef */
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -15,9 +17,9 @@ function App() {
     });
   }
   useEffect(() => {
-    test2();
+    getQRcodeFromScreen();
   }, []);
-  function test2() {
+  function getQRcodeFromScreen() {
     chrome.tabs.captureVisibleTab(
       null,
       { format: "png" /* png, jpeg */, quality: 80 },
@@ -31,13 +33,15 @@ function App() {
             })
             .catch((err) => {
               setMessage(err);
-              console.log(err || "no QR code found");
+              console.log(
+                err || "no QR code found, Would you like to refresh ?"
+              );
             });
         } else {
           // Grab failed, warning
           // To handle issues like permissions - https://github.com/folletto/Blipshot/issues/9
           alert(
-            "I'm sorry.\n\nIt seems app wasn't able to grab the screenshot of the active tab.\n\nPlease check the extension permissions.\n\nIf the problem persists contact me at \nhttp://github.com/folletto/Blipshot/issues\n\nError: " +
+            "I'm sorry.\n\nIt seems the extension wasn't able to grab the screenshot of the active tab. Error: " +
               chrome.runtime.lastError.message +
               "\n\n"
           );
@@ -46,13 +50,37 @@ function App() {
       }
     );
   }
-
+  function generateQR() {
+    QRCode.toDataURL(text)
+      .then((url) => {
+        console.log(url);
+        setImage(url);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
   return (
     <div className="App">
       {/* <button onClick={test}>Send alert</button>
       <div>{id}</div>
-      <button onClick={test2}>Send message</button> */}
+      */}
       <div>{message}</div>
+      <button onClick={getQRcodeFromScreen}>Refresh</button>
+
+      <form>
+        <label>
+          Enter Text:
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </label>
+      </form>
+      <button onClick={generateQR}>Generate QR</button>
+      <div>{image}</div>
+      <img src={image} />
     </div>
   );
 }
